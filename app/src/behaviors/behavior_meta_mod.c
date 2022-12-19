86d2708c650b90e6860d7b71e9b500c0bb82da94
Sam Vasta <hello@samvasta.com>
2022-12-19 04:06:02 +0000

    meta mods!!!

--- app/src/behaviors/behavior_meta_mod.c
+++ app/src/behaviors/behavior_meta_mod.c
@@ -9,91 +9,38 @@
 #include <device.h>
 #include <drivers/behavior.h>
 #include <logging/log.h>
-#include <zmk/behavior.h>
 
-#include <zmk/matrix.h>
-#include <zmk/endpoints.h>
 #include <zmk/event_manager.h>
-#include <zmk/events/position_state_changed.h>
 #include <zmk/events/keycode_state_changed.h>
-#include <zmk/events/modifiers_state_changed.h>
-#include <zmk/hid.h>
+#include <zmk/behavior.h>
+#include <zmk/meta.h>
 
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
-#if DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT)
+static int behavior_meta_mod_init(const struct device *dev) { return 0; };
 
-struct behavior_meta_mod_config {
-    struct zmk_behavior_binding normal_binding;
-    struct zmk_behavior_binding morph_binding;
-    zmk_mod_flags_t mods;
-};
+static int on_keymap_binding_pressed(struct zmk_behavior_binding *binding,
+                                     struct zmk_behavior_binding_event event) {
+    uint32_t key = zmk_meta_mode() == 0 ? binding->param1 : binding->param2;
 
-struct behavior_meta_mod_data {
-    struct zmk_behavior_binding *pressed_binding;
-};
-
-static int on_meta_mod_binding_pressed(struct zmk_behavior_binding *binding,
-                                        struct zmk_behavior_binding_event event) {
-    const struct device *dev = device_get_binding(binding->behavior_dev);
-    const struct behavior_meta_mod_config *cfg = dev->config;
-    struct behavior_meta_mod_data *data = dev->data;
-
-    if (data->pressed_binding != NULL) {
-        LOG_ERR("Can't press the same mod-morph twice");
-        return -ENOTSUP;
-    }
-
-    if (zmk_hid_get_explicit_mods() & cfg->mods) {
-        data->pressed_binding = (struct zmk_behavior_binding *)&cfg->morph_binding;
-    } else {
-        data->pressed_binding = (struct zmk_behavior_binding *)&cfg->normal_binding;
-    }
-    return behavior_keymap_binding_pressed(data->pressed_binding, event);
+    LOG_DBG("position %d keycode 0x%02X", event.position, key);
+    return ZMK_EVENT_RAISE(
+        zmk_keycode_state_changed_from_encoded(key, true, event.timestamp));
 }
 
-static int on_meta_mod_binding_released(struct zmk_behavior_binding *binding,
-                                         struct zmk_behavior_binding_event event) {
-    const struct device *dev = device_get_binding(binding->behavior_dev);
-    struct behavior_meta_mod_data *data = dev->data;
-
-    if (data->pressed_binding == NULL) {
-        LOG_ERR("Mod-morph already released");
-        return -ENOTSUP;
-    }
-
-    struct zmk_behavior_binding *pressed_binding = data->pressed_binding;
-    data->pressed_binding = NULL;
-    return behavior_keymap_binding_released(pressed_binding, event);
+static int on_keymap_binding_released(struct zmk_behavior_binding *binding,
+                                      struct zmk_behavior_binding_event event) {
+    uint32_t key = zmk_meta_mode() == 0 ? binding->param1 : binding->param2;
+    LOG_DBG("position %d keycode 0x%02X", event.position, key);
+    return ZMK_EVENT_RAISE(
+        zmk_keycode_state_changed_from_encoded(key, false, event.timestamp));
 }
 
 static const struct behavior_driver_api behavior_meta_mod_driver_api = {
-    .binding_pressed = on_meta_mod_binding_pressed,
-    .binding_released = on_meta_mod_binding_released,
-};
-
-static int behavior_meta_mod_init(const struct device *dev) { return 0; }
-
-#define _TRANSFORM_ENTRY(idx, node)                                                                \
-    {                                                                                              \
-        .behavior_dev = DT_LABEL(DT_INST_PHANDLE_BY_IDX(node, bindings, idx)),                     \
-        .param1 = COND_CODE_0(DT_INST_PHA_HAS_CELL_AT_IDX(node, bindings, idx, param1), (0),       \
-                              (DT_INST_PHA_BY_IDX(node, bindings, idx, param1))),                  \
-        .param2 = COND_CODE_0(DT_INST_PHA_HAS_CELL_AT_IDX(node, bindings, idx, param2), (0),       \
-                              (DT_INST_PHA_BY_IDX(node, bindings, idx, param2))),                  \
-    }
+    .binding_pressed = on_keymap_binding_pressed, .binding_released = on_keymap_binding_released};
 
 #define KP_INST(n)                                                                                 \
-    static struct behavior_meta_mod_config behavior_meta_mod_config_##n = {                      \
-        .normal_binding = _TRANSFORM_ENTRY(0, n),                                                  \
-        .morph_binding = _TRANSFORM_ENTRY(1, n),                                                   \
-        .mods = DT_INST_PROP(n, mods),                                                             \
-    };                                                                                             \
-    static struct behavior_meta_mod_data behavior_meta_mod_data_##n = {};                        \
-    DEVICE_DT_INST_DEFINE(n, behavior_meta_mod_init, NULL, &behavior_meta_mod_data_##n,          \
-                          &behavior_meta_mod_config_##n, APPLICATION,                             \
+    DEVICE_DT_INST_DEFINE(n, behavior_meta_mod_init, NULL, NULL, NULL, APPLICATION,                \
                           CONFIG_KERNEL_INIT_PRIORITY_DEFAULT, &behavior_meta_mod_driver_api);
 
 DT_INST_FOREACH_STATUS_OKAY(KP_INST)
-
-#endif
---
+++ app/src/behaviors/behavior_meta_mod_set.c
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2022 The ZMK Contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#define DT_DRV_COMPAT zmk_behavior_meta_mod_set
+
+#include <device.h>
+#include <drivers/behavior.h>
+#include <logging/log.h>
+#include <zmk/behavior.h>
+
+#include <zmk/matrix.h>
+#include <zmk/endpoints.h>
+#include <zmk/event_manager.h>
+#include <zmk/events/position_state_changed.h>
+#include <zmk/events/keycode_state_changed.h>
+#include <zmk/events/modifiers_state_changed.h>
+#include <zmk/hid.h>
+#include <zmk/meta.h>
+
+LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
+
+#if DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT)
+
+static int on_meta_mod_set_binding_pressed(struct zmk_behavior_binding *binding,
+                                        struct zmk_behavior_binding_event event) {
+    
+    zmk_meta_set(binding->param1);
+
+    return ZMK_BEHAVIOR_OPAQUE;
+}
+
+static int on_meta_mod_set_binding_released(struct zmk_behavior_binding *binding,
+                                         struct zmk_behavior_binding_event event) {
+    return ZMK_BEHAVIOR_OPAQUE;
+}
+
+static const struct behavior_driver_api behavior_meta_mod_set_driver_api = {
+    .binding_pressed = on_meta_mod_set_binding_pressed,
+    .binding_released = on_meta_mod_set_binding_released,
+};
+
+static int behavior_meta_mod_set_init(const struct device *dev) { return 0; }
+
+#define META_MOD_SET_INST(n)                                                                                 \
+    DEVICE_DT_INST_DEFINE(n, behavior_meta_mod_set_init, NULL, NULL, NULL, APPLICATION,               \
+                          CONFIG_KERNEL_INIT_PRIORITY_DEFAULT, &behavior_meta_mod_set_driver_api);
+
+DT_INST_FOREACH_STATUS_OKAY(META_MOD_SET_INST)
+
+#endif
