ffc257ef0c203584104bdf396dfdc348da50297e
Sam Vasta <hello@samvasta.com>
2022-12-19 04:06:02 +0000

    meta mods!!!

---
+++ app/include/zmk/meta.h
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2022 The ZMK Contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#pragma once
+
+int zmk_meta_mode();
+int zmk_meta_set(uint8_t meta_value);
---
+++ app/src/meta.c
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2022 The ZMK Contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <device.h>
+#include <init.h>
+
+#include <errno.h>
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#if IS_ENABLED(CONFIG_SETTINGS)
+
+#include <settings/settings.h>
+
+#endif
+
+#include <zmk/meta.h>
+
+static uint8_t meta;
+
+int zmk_meta_set(uint8_t value) {
+  meta = value;
+  return 0;
+};
+
+int zmk_meta_mode() {
+  return meta;
+};
