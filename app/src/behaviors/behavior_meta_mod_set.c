040005f6f1806b751793e695d9716e58120f1847
Sam Vasta <hello@samvasta.com>
2023-05-29 23:17:32 +0000

    fix formatting I guess

--- app/src/behaviors/behavior_meta_mod_set.c
+++ app/src/behaviors/behavior_meta_mod_set.c
@@ -25,15 +25,15 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 #if DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT)
 
 static int on_meta_mod_set_binding_pressed(struct zmk_behavior_binding *binding,
-                                        struct zmk_behavior_binding_event event) {
-    
+                                           struct zmk_behavior_binding_event event) {
+
     zmk_meta_set(binding->param1);
 
     return ZMK_BEHAVIOR_OPAQUE;
 }
 
 static int on_meta_mod_set_binding_released(struct zmk_behavior_binding *binding,
-                                         struct zmk_behavior_binding_event event) {
+                                            struct zmk_behavior_binding_event event) {
     return ZMK_BEHAVIOR_OPAQUE;
 }
 
@@ -44,8 +44,8 @@ static const struct behavior_driver_api behavior_meta_mod_set_driver_api = {
 
 static int behavior_meta_mod_set_init(const struct device *dev) { return 0; }
 
-#define META_MOD_SET_INST(n)                                                                                 \
-    DEVICE_DT_INST_DEFINE(n, behavior_meta_mod_set_init, NULL, NULL, NULL, APPLICATION,               \
+#define META_MOD_SET_INST(n)                                                                       \
+    DEVICE_DT_INST_DEFINE(n, behavior_meta_mod_set_init, NULL, NULL, NULL, APPLICATION,            \
                           CONFIG_KERNEL_INIT_PRIORITY_DEFAULT, &behavior_meta_mod_set_driver_api);
 
 DT_INST_FOREACH_STATUS_OKAY(META_MOD_SET_INST)
